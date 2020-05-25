23c88c0ceec9329760873591f79df94000672621
kunalpratapsingh13 <40957858+kunalpratapsingh13@users.noreply.github.com>
2020-05-25 09:14:54 +0530

    Update QuickSort.java

--- QuickSort.java
+++ QuickSort.java
@@ -1,74 +1,54 @@
-import java.util.*; 
-public class QuickSort 
-{ 
+import java.util.*;
+public class Main {
 
-	public static int partition(int arr[], int low, int high) 
-	{ 
-		int pivot = arr[high]; 
-		int i = (low-1); // index of smaller element 
-		for (int j=low; j<high; j++) 
-		{ 
-			// If current element is smaller than the pivot 
-			if (arr[j] < pivot) 
-			{ 
-				i++; 
+	 public static int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1);
+        for (int j=low; j<high; j++) 
+        {  
+            if (arr[j] < pivot) 
+            { 
+                i++;  
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    } 
 
-				// swap arr[i] and arr[j] 
-				int temp = arr[i]; 
-				arr[i] = arr[j]; 
-				arr[j] = temp; 
-			} 
-		} 
-
-		// swap arr[i+1] and arr[high] (or pivot) 
-		int temp = arr[i+1]; 
-		arr[i+1] = arr[high]; 
-		arr[high] = temp; 
-
-		return i+1; 
-	} 
-
-
-	/* The main function that implements QuickSort() 
-	arr[] --> Array to be sorted, 
-	low --> Starting index, 
-	high --> Ending index */
 	public static void sort(int arr[], int low, int high) 
-	{ 
-		if (low < high) 
-		{ 
-			/* pi is partitioning index, arr[pi] is 
-			now at right place */
-			int pi = partition(arr, low, high); 
+    { 
+        if (low < high) 
+        { 
+            int pi = partition(arr, low, high);  
+            sort(arr, low, pi-1); 
+            sort(arr, pi+1, high); 
+        } 
+    } 
 
-			// Recursively sort elements before 
-			// partition and after partition 
-			sort(arr, low, pi-1); 
-			sort(arr, pi+1, high); 
-		} 
-	} 
-
-	/* A utility function to print array of size n */
 	public static void printArray(int arr[]) 
-	{ 
-		int n = arr.length; 
-		for (int i=0; i<n; ++i) 
-			System.out.print(arr[i]+" "); 
-		System.out.println(); 
-	} 
+    { 
+        int n = arr.length; 
+        for (int i=0; i<n; ++i) 
+            System.out.print(arr[i]+" "); 
+        System.out.println(); 
+    } 
 
-	// Driver program 
-	public static void main(String args[]) 
-	{ 
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] arr = new int[N];
-		for (int i=0;i<=N-1;i++){
-			arr[i]=sc.nextInt();
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		for (int i=0;i<n;i++){
+			arr[i] = sc.nextInt();
 		}
-		QuickSort ob = new QuickSort(); 
-		ob.sort(arr, 0, N-1);  
-		printArray(arr); 
-	} 
-} 
-/*This code is contributed by Rajat Mishra */
+
+		sort(arr,0,arr.length - 1);
+		printArray(arr);
+    }
+}
